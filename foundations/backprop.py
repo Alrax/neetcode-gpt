45ae02841b2e00b4380861ef1f48d53bec812dfa
Alex Ramallo <71142809+Alrax@users.noreply.github.com>
2026-06-18 16:19:22 +0200

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -4,6 +4,9 @@ from typing import Tuple
 
 
 class Solution:
+    def sigmoid(self, z):
+        return  1 / (1 + np.exp(-z))
+
     def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
         # x: 1D input array
         # w: 1D weight array
@@ -11,13 +14,15 @@ class Solution:
         # y_true: true target value
         #
         # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
-        # Loss: L = 0.5 * (y_hat - y_true)^2
-        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
         z = np.dot(x, w) + b
-        y_hat = 1 / (1 + np.exp(-z))
-        L = 0.5 * (y_hat - y_true) ** 2
+        y_hat = self.sigmoid(z)
+
+        # Loss: L = 0.5 * (y_hat - y_true)^2
+        # dL_db = dL_dy_hat * dy_hat_dz *​ dz_db = (y_hat - y) * y_hat(1 - y_hat) * 1
+        # dL_dw = dL_dy_hat * dy_hat_dz *​ dz_dw​ = (y_hat - y) * y_hat(1 - y_hat) * x
 
         dL_db = (y_hat - y_true) * y_hat * (1 - y_hat)
         dL_dw = ((y_hat - y_true) * y_hat * (1 - y_hat)) * x
 
+        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
         return (np.round(dL_dw, 5), np.round(dL_db, 5))
